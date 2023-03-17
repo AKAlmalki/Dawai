59bebc0282e99854f153c8e9e4c622c7fe8b1d95
AKAlmalki <ahmed.k.1454@gmail.com>
2023-03-17 18:27:24 +0300

    Third commit

--- config.py
+++ config.py
@@ -1,19 +1,13 @@
 import os
-# SECRET_KEY = os.urandom(32)
-SECRET_KEY = "Dawai-2000"
+
+SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database AND setting up the env variables
-DB_HOST = os.getenv('DB_HOST', 'database-dawai.cyd5dayxhyym.me-south-1.rds.amazonaws.com:5432')
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'Dawai-2000')
-DB_NAME = os.getenv('DB_NAME', 'dawai')
-
-DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_PATH = os.environ['DATABASE_URL']
 
 # IMPLEMENT DATABASE URL [done]
 SQLALCHEMY_DATABASE_URI = DB_PATH
